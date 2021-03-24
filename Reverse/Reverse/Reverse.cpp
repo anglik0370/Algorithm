93e38fb57d37e525a7ca4e798203f074b07d20be
Anglik <seonwoo6429@gmail.com>
2021-03-24 16:11:47 +0900

    거꾸로 출력 스크립트 연습

---
+++ Reverse/Reverse/Reverse.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char input[3][50] = { ' ', };
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "문자열을 입력하세요 : ";
+        cin.getline(input[i], 50);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "변환된" <<i + 1 << "번째 문자열 : ";
+        for (int j = strlen(input[i]); j >= 0; j--)
+        {
+            cout << input[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
