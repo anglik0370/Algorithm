cd8335e3163f430096b8855741242726fbd520a3
Anglik <seonwoo6429@gmail.com>
2021-03-26 00:36:58 +0900

    재귀함수 실습

---
+++ 재귀함수/RecursionFunction Learning/RecursionFunction Learning/RecursionFunction Learning.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Factorial(int n);
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << endl << "1부터 " << n << "까지의 펙토리얼은" << Factorial(n) << endl;
+}
+
+int Factorial(int n)
+{
+    int result = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+}
