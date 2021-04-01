86e4f71a8ad1034d7a423dec0713c50f9f6e2569
Anglik <seonwoo6429@gmail.com>
2021-04-01 19:16:45 +0900

    재귀함수 4 실습

---
+++ n-1 출력/n-1 출력/n-1 출력.cpp
@@ -0,0 +1,27 @@
+﻿//20108 박선우
+
+#include <iostream>
+
+using namespace std;
+
+int maxReverse(int max, int min)
+{
+    if (max < min) return 0;
+
+    else if (max % 2 == 1)
+    {
+        cout << max << endl;
+    }
+
+    return maxReverse(max - 1, min);
+}
+
+int main()
+{
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    maxReverse(a, b);
+}
